346832e7d6e067d1c4a894b1889d2ce64def2935
Sergio Morales <i.serge23@gmail.com>
2015-12-10 14:51:44 -0600

    Refactor 'var' to 'let' in day 2

--- 02/1.js
+++ 02/1.js
@@ -1,15 +1,15 @@
 import numberSortComparator from '../lib/numberSortComparator';
 
 export default function wrappingPaper(data) {
-  var presents = data.split('\n');
+  let presents = data.split('\n');
   return presents.reduce(function(totalSquareFeet, present) {
-    var dimensions = present.split('x').map(function(x) { return parseInt(x); });
-    var sidesSquareFeet = [
+    let dimensions = present.split('x').map(function(x) { return parseInt(x); });
+    let sidesSquareFeet = [
       dimensions[0] * dimensions[1],
       dimensions[0] * dimensions[2],
       dimensions[1] * dimensions[2]
     ].sort(numberSortComparator);
-    var presentSquareFeet = sidesSquareFeet[0] * 3 + sidesSquareFeet[1] * 2 + sidesSquareFeet[2] * 2;
+    let presentSquareFeet = sidesSquareFeet[0] * 3 + sidesSquareFeet[1] * 2 + sidesSquareFeet[2] * 2;
     return presentSquareFeet + totalSquareFeet;
   }, 0);
 }
--- 02/2.js
+++ 02/2.js
@@ -1,11 +1,11 @@
 import numberSortComparator from '../lib/numberSortComparator';
 
 export default function ribbon(data) {
-  var presents = data.split('\n');
+  let presents = data.split('\n');
   return presents.reduce(function(totalFeet, present) {
-    var dimensions = present.split('x').map(function(x) { return parseInt(x); }).sort(numberSortComparator);
-    var shortestPerimeter = dimensions[0] * 2 + dimensions[1] * 2;
-    var ribbon = shortestPerimeter + dimensions[0] * dimensions[1] * dimensions[2];
+    let dimensions = present.split('x').map(function(x) { return parseInt(x); }).sort(numberSortComparator);
+    let shortestPerimeter = dimensions[0] * 2 + dimensions[1] * 2;
+    let ribbon = shortestPerimeter + dimensions[0] * dimensions[1] * dimensions[2];
     return totalFeet + ribbon;
   }, 0);
 }
