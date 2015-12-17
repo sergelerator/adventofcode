0e50061e3819f5717db5d65f98eea50118b9fd40
Sergio Morales <i.serge23@gmail.com>
2015-12-16 23:01:57 -0600

    Day 07, part 1

---
+++ 07/1.js
@@ -0,0 +1,78 @@
+function getSignalFrom(wires, wire) {
+  let signal = wires[wire];
+
+  if (signal === undefined) {
+    return ~~wire;
+  }
+  if (typeof signal === 'function') {
+    wires[wire] = signal();
+  }
+  return wires[wire];
+}
+
+function directSignal(wires, input) {
+  return getSignalFrom(wires, input);
+}
+
+function andGate(wires, leftSide, rightSide) {
+  return getSignalFrom(wires, leftSide) & getSignalFrom(wires, rightSide);
+}
+
+function orGate(wires, leftSide, rightSide) {
+  return getSignalFrom(wires, leftSide) | getSignalFrom(wires, rightSide);
+}
+
+function notGate(wires, input) {
+  return ~getSignalFrom(wires, input) & 0xFFFF;
+}
+
+function leftShift(wires, input, bits) {
+  return getSignalFrom(wires, input) << ~~bits;
+}
+
+function rightShift(wires, input, bits) {
+  return getSignalFrom(wires, input) >> ~~bits;
+}
+
+const gates = {
+  'AND': andGate,
+  'OR': orGate,
+  'NOT': notGate,
+  'LSHIFT': leftShift,
+  'RSHIFT': rightShift
+};
+
+function mapWires(data) {
+  let wires = {};
+
+  data.split('\n').forEach((instruction) => {
+    let tokens = instruction.split(' ');
+    let gate, input = [], wire = tokens.pop();
+    tokens.pop(); // No need for the arrow token
+
+    tokens.forEach((token) => {
+      if (gates[token]) {
+        gate = gates[token];
+      } else {
+        input.push(token);
+      }
+    });
+
+    gate || (gate = directSignal);
+    wires[wire] = () => gate.apply(null, [wires, ...input]);
+  });
+
+  return wires;
+}
+
+function resolveCircuit(wires) {
+  for (let wire in wires) {
+    wires[wire] = getSignalFrom(wires, wire);
+  }
+}
+
+export default function wiresSolution(data) {
+  const circuit = mapWires(data);
+  resolveCircuit(circuit);
+  return circuit.a;
+}
